fb8607620f4ff596716327e11342e28d82738929
Mark Petersen <atarihomestar@gmail.com>
2023-10-10 17:10:21 -0600

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import WordPrompter from "./WordPrompter";
 
 const App = () => {
--- src/WordPrompter.js
+++ src/WordPrompter.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import Stats from "./Stats";
 

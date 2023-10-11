267f0634cd85fffb8b19ed7a29b1644d3a76e3bb
Mark Petersen <atarihomestar@gmail.com>
2023-10-11 12:38:26 -0600

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import WordPrompter from "./WordPrompter";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const App = () => {
   const wordList = ["apple", "banana"];
 
   return (
-    <div>
-      <WordPrompter words={wordList} />
-    </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar sx={{ backgroundColor: "green" }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Speedy Speller
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <WordPrompter words={wordList} />
+      </div>
+    </>
   );
 };
 
